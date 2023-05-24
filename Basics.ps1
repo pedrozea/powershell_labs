f4b38fd33090426e7d29bfd56b4bf779fdeb3c94
Pedro Alejandro Zea Rodriguez <pedro.zea@globant.com>
2023-05-24 10:49:13 -0500

    [DevOps][feature]:Uploading scripts worked on in the local lab Changes committed:        new file:   Basics.ps1        new file:   Objects.ps1

---
+++ Basics.ps1
@@ -0,0 +1,27 @@
+﻿#Declaracion de variables
+$a = 5
+$b = 4
+$palabra1 = "a"
+$palabra2 = "A"
+$palabra3 = "Apple"
+$frase1 = "My name is Alejo"
+
+#Operaciones con variables
+$a -lt $b
+$a -gt $b
+$a -ne $b
+$a -eq $b
+
+#Operaciones con texto
+$palabra1 -eq $palabra2
+$palabra1 -ceq $palabra2
+$palabra3 -eq "A*"
+$palabra3 -like "A*"
+$palabra3 -like "*p*"
+$palabra3 -like "A??le"
+$frase1 -match "Alejo"
+$frase1 -cmatch "Alejo"
+
+Get-Alias #Permite visualizar los Alias existentes y su correpondiente Cmdlet
+
+Set-Alias #Para crear un nuevo alias
---
+++ Objects.ps1
@@ -0,0 +1,63 @@
+﻿###PowerShell retorna todo como un objeto
+
+Get-Member #En PowerShell, casi todo es un objeto, lo que significa que cada elemento tiene propiedades (datos sobre el objeto) y métodos (acciones que puedes realizar con el objeto). Este comando ayuda a inspeccionar estos objetos y ver qué propiedades y métodos están disponibles.
+
+gm
+
+help Get-ChildItem -Online
+
+
+$MisArchivos = Get-ChildItem
+
+Write-Host "En el directorio actual tienes" $MisArchivos.Count "Carpetas"
+
+
+###Sort para ordenar los objetos de acuerdo a una propiedad
+
+$MisArchivos |sort -Property LastWriteTime -Descending
+
+
+###Where para filtrar objetos por propiedades
+
+$MisArchivos | Where-Object {$_.Name -like ".b*"} | sort -Descending
+
+$MisArchivos | Where-Object {$_.Length -gt 1312}
+
+
+$MisCarpetas | Sort-Object -Property CreationTime | Format-Table Name,CreationTime
+
+$MisArchivos | Where-Object {($_.length -gt 100000) -and ($_.Name -like "D*")}
+
+$MisArchivos | sort -Property CreationTime -Descending | Format-Table Name,CreationTime
+
+
+#Iteraciones con foreach
+
+1..10
+
+1.10 |foreach {$_*2}
+
+1..10 |foreach {$_*2}
+
+1..10 |foreach {"$_" + " -"}
+
+1..10 |foreach {"Iteración No. $_"}
+
+1..10 |foreach {"*"*$_}
+
+1..10 | foreach {if ($_%2){"El numero $_ es impar"}}
+
+###Strings
+
+$stringcomputers = @("Server1","Server2","Server3")
+
+$stringcomputers[2]
+
+$stringcomputers[2] = "Server4"
+
+$stringcomputers |foreach {$_ + " tiene " + $_.length + " caracteres"}
+
+$stringlaptops = @("Laptop1","Laptop2")
+
+$equipos = $stringcomputers + $stringlaptops
+
